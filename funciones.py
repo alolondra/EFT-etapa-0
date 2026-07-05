d06fc50275621612ec119b168811b5138e3fbcf3
alolondra <alo.molinav@duocuc.cl>
2026-07-05 23:29:32 +0000

    Se implementan funciones de validación

--- funciones.py
+++ funciones.py
@@ -1,5 +1,36 @@
 def leer_opcion():
-    pass
+    while True:
+        try:
+            opcion=int(input('Ingrese opción: '))
+            if opcion >= 1 and opcion <= 7:
+                return opcion
+            else:
+                print('Debe seleccionar una opción válida.')
+        except ValueError:
+            print('Debe ingresar un número entero.')
+
+def validar_codigo(codigo, productos):
+    codigo = codigo.strip().upper()
+    return codigo != '' and codigo not in productos
+
+def validar_nombre(nombre):
+    return nombre.strip() != ''
+
+def validar_categoria(categoria):
+    return categoria.strip() != ''
+
+def validar_precio(precio):
+    return precio > 0
+
+def validar_disponible(opcion):
+    opcion = opcion.lower()
+    return opcion == 's' or opcion == 'n'
+
+def validar_stock(stock):
+    return stock >= 0
+
+def validar_vendidos(vendidos):
+    return vendidos >= 0
 
 def stock_categoria(categoria,productos,inventario):
     pass

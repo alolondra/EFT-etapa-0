8730e218f3fae7b123cfbe6b5993a61ccf1060f8
alolondra <alo.molinav@duocuc.cl>
2026-07-05 05:50:31 +0000

    Se agregan funciones principales del sistema

---
+++ funciones.py
@@ -0,0 +1,23 @@
+def leer_opcion():
+    pass
+
+def stock_categoria(categoria,productos,inventario):
+    pass
+
+def buscar_precio(precio_min,precio_max,productos,inventario):
+    pass
+
+def buscar_codigo(codigo,productos):
+    pass
+
+def actualizar_precio(codigo,nuevo_precio,productos):
+    pass
+
+def agregar_producto(codigo,nombre,categoria,precio,disponible,stock,vendidos,productos,inventario):
+    pass
+
+def eliminar_producto(codigo,productos,inventario):
+    pass
+
+def mostrar_productos(productos,inventario):
+    pass
